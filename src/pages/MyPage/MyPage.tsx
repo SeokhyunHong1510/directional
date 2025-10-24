278890d940738554ce680a053709dded975a466a
seokhyunhong1510 <hsh1510@gmail.com>
2025-10-24 12:30:34 +0900

    멘트 수정

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -87,11 +87,7 @@ const MyPage = () => {
   };
 
   const handleDeleteAll = async () => {
-    if (
-      window.confirm(
-        '정말로 모든 게시글을 삭제하시겠습니까? 이 작업은 되돌릴 수 없습니다.',
-      )
-    ) {
+    if (window.confirm('모든 게시글을 삭제하시겠습니까?')) {
       try {
         await deleteAllPosts();
         alert('모든 게시글이 삭제되었습니다.');
